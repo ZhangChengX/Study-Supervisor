4e4167f3bb26c6a8a9aa2832f78e39b5ee9a177f
zhaoqianlu <zhaoqian_lu@student.uml.edu>
2014-11-07 10:15:16 -0500

    change the tables

--- KeepStudy/app/src/main/java/com/example/sunxinzi/keepstudy/DataBaseOpenHelper.java
+++ KeepStudy/app/src/main/java/com/example/sunxinzi/keepstudy/DataBaseOpenHelper.java
@@ -11,9 +11,10 @@ import android.util.Log;
 public class DataBaseOpenHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "myDB.db";
     private static final int DATABASE_VERSION = 2;
-    private static final String COURSE_TABLE_CREATE = "create table course (course_name text NOT NULL primary key, grade int, remark text);";
-    private static final String STUDY_RECORDS_TABLE_CREATE = "create table study (ID int NOT NULL primary key autoincrement," +
-            " c_name text, start_time  timestamp, end_time timestamp, study_time_length REAL, planned_time_length REAL, " +
+    private static final String COURSE_TABLE_CREATE = "create table Course (ID int NOT NULL primary key autoincrement,course_name text, grade int, remark text);";
+    private static final String STUDY_RECORDS_TABLE_CREATE = "create table Study(ID int NOT NULL primary key autoincrement," +
+            "c_id int, c_name text, start_time  timestamp, end_time timestamp, " +
+            "study_time_length REAL, planned_time_length REAL, " +
             "latitude real, longitude real, remark text)";
 
     DataBaseOpenHelper(Context context){
