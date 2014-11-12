6f618b2ad25adb4dcaf13ecd92eee06b3c40ab35
Cheng Zhang <zhangchengx@gmail.com>
2014-11-12 15:59:05 -0500

    add reportHelper

---
+++ KeepStudy/app/src/main/java/com/example/sunxinzi/keepstudy/reportHelper.java
@@ -0,0 +1,34 @@
+package com.example.sunxinzi.keepstudy;
+
+
+/**
+ * Created by Cheng on 11/12/2014.
+ */
+public class reportHelper {
+
+    /**
+     * get all courses and grades
+     */
+    public void getCourses() {
+
+    }
+
+    /**
+     *
+     * @param startDate
+     * @param endDate
+     */
+    public void getTotalHours(String startDate, String endDate) {
+
+    }
+
+    /**
+     * 
+     * @param startDate
+     * @param endDate
+     */
+    public void getLocationSet(String startDate, String endDate) {
+
+    }
+
+}
